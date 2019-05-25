b7fa5f6dea239605e6248225e80d542dc6f589fd
dh <dh@dh-RV411-RV511-E3511-S3511-RV711-E3411>
2019-05-26 01:44:53 +0900

    initial_commit

---
+++ src/Calculator01.java
@@ -0,0 +1,16 @@
+
+public class Calculator01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("덧셈 : " + (4 + 3));
+		System.out.println("뺼셈 : " + (4 - 3));
+		System.out.println("곱셈 : " + (4 * 3));
+		System.out.println("나눗셈 : " + (4 / 3));
+		
+		
+		
+	}
+	
+	
+}
